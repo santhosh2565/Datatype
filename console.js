8d1e284142ffccc9f808b24f43b95b79002c53a5
Santhosh Kumar <126172456+santhosh2565@users.noreply.github.com>
2026-02-01 21:54:39 +0530

    Add files via upload

---
+++ console.js
@@ -0,0 +1,23 @@
+        let firstname = "santhosh"
+        console.log("value of the variable is",firstnamename)
+        console.log("datatype:",typeof(firstnamename))
+
+        let age = 24
+        console.log("value of the variable is",age)
+        console.log("datatype:",typeof(age))
+
+        let number = 23456789876543n
+        console.log("value of the variable is",number)
+        console.log("datatype:",typeof(number))
+
+        let answer = true
+        console.log ("value of the variable is",answer)
+        console.log ("datatype:",typeof(answer))
+        
+        let value = null
+        console.log ("value of the variable is",value)
+        console.log("datatype:",typeof(value))
+
+        let mobiles = ["vivo","apple","oneplus"]
+        console.log ("value of the variables is",mobiles)
+        console.log ("datatype:",typeof(mobiles))
